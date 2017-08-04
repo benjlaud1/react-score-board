7e112b52fe5dd11ddb9f021ac84b4fd4d2d2aed7
Ben Lauderbaugh <benjlaud1@gmail.com>
2017-08-04 15:22:43 -0500

    added stats to the header

---
+++ client/components/Stats.jsx
@@ -0,0 +1,30 @@
+/*
+    ./client/components/Stats.jsx
+*/
+
+import React from 'react';
+
+export default class Stats extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <table className="stats">
+                <tbody>
+                    <tr>
+                        <td>Players:</td>
+                        <td>{this.props.playerCount}</td>
+                    </tr>
+                    <tr>
+                        <td>Total Points:</td>
+                        <td>123</td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
